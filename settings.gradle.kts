e52c1e4f4326bc470147e3a37ffca1d2088ff277
prep0ster0us <rbabu1@unh.newhaven.edu>
2023-09-23 16:56:05 -0400

    Initial Commit
    Created project and added splash screen xml

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "rbabu_demoapp"
+include(":app")
+ 
