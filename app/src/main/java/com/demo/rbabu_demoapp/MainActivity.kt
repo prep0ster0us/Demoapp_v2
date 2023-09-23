830aba75eb2c842e784db68f43729d181e80f232
prep0ster0us <rbabu1@unh.newhaven.edu>
2023-09-23 17:46:45 -0400

    Completed main activity layout
    added all elements and spaced them out for cleaner front end

--- app/src/main/java/com/demo/rbabu_demoapp/MainActivity.kt
+++ app/src/main/java/com/demo/rbabu_demoapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.demo.rbabu_demoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.demo.rbabu_demoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // by default, set visibility of question text, answer field and submit button to be invisible (toggle visibility when random question button is pressed)
+        binding.txtQuestion.visibility = View.INVISIBLE
+        binding.txtAnswer.visibility = View.INVISIBLE
+        binding.btnSubmit.visibility = View.INVISIBLE
     }
 }
