9cee3a47bcfc07cb7191c25438d0e0363351bcf6
prep0ster0us <rbabu1@unh.newhaven.edu>
2023-09-23 17:05:01 -0400

    Added splash screen
    added animation for splash screen and intent to main

--- app/src/main/java/com/demo/rbabu_demoapp/SplashScreenActivity.kt
+++ app/src/main/java/com/demo/rbabu_demoapp/SplashScreenActivity.kt
@@ -1,11 +1,32 @@
 package com.demo.rbabu_demoapp
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import com.demo.rbabu_demoapp.databinding.ActivitySplashScreenBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        val binding: ActivitySplashScreenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.splashImage.alpha = 0f
+        binding.splashImage.animate()
+            .setDuration(2500)
+            .alpha(1f)
+
+        binding.splashText.alpha = 0f
+        binding.splashText.animate()
+            .setDuration(2500)
+            .alpha(1f)
+            .withEndAction {
+                val intentMain = Intent(this, MainActivity::class.java)
+                startActivity(intentMain)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            }
     }
 }
